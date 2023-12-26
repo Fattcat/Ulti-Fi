c4e4bfe3f0091067bfb56b3fb4c36be20cff0f9e
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-12-26 22:22:58 +0100

    Update Ulti-Fi.py

--- Ulti-Fi.py
+++ Ulti-Fi.py
@@ -9,6 +9,7 @@ import os
 import time
 import subprocess
 import platform
+from SRC import DoNK-Menu
 
 # Farbicky
 reset = '\033[0m'
@@ -28,6 +29,28 @@ time.sleep(1)
 #TU SPRAVIT LEPSI CHECK OS
 # ------------------------
 
+def check_monitor_mode():
+    # Spustí príkaz iwconfig a zachytí výstup ze neho musim nastavit text=False az to nepise vsetko
+    result = subprocess.run(["iwconfig"], capture_output=True, text=True)
+
+    # Získa výstupné riadky z teho
+    output_lines = result.stdout.split('\n')
+
+    # Prejde cez riadky a vyhľada informácie o tom monitor móde
+    for line in output_lines:
+        if "Mode:Monitor" in line:
+            # Extrahujte názov wifi adaptéra
+            adapter_name = line.split()[0]
+            
+            # Vypíše informáciu o zapnutom monitor móde
+            print(f"Monitor mód je už zapnutý na: {adapter_name}")
+        else:
+          print("Monitor mod este neni zapnuty")
+# Spust funkciu pre kontrolu monitor módu
+print("Checking if Monitor Mode is Turned ON ...\n")
+check_monitor_mode()
+
+#spušta iba testovaci prikaz len tak :D
 os.system("iwconfig")
 AdapterChoice = input("Select WiFi Adapter for USE : ")
 print("Starting monitor mode on", AdapterChoice)
