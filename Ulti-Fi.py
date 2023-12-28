9d194625a506ed54c67defe41f4f43e58c1a8279
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-12-28 15:46:34 +0000

    Update Ulti-Fi.py

--- Ulti-Fi.py
+++ Ulti-Fi.py
@@ -4,68 +4,94 @@
 #           Created for automate penetration testing
 # ------------------------------------------------------------- #
 
-# Moduly
+# ------ [ Moduly ] ------ #
 import os
 import time
+import random
 import subprocess
 import platform
+# ------ [ My  Modules ] ------ #
 from SRC import DoNKModule
+from SRC import CaptureHandShake
+from SRC import Deauth
+# ------ [ My  Modules ] ------ #
+
 
 # Farbicky
 reset = '\033[0m'
 red = '\033[31m'
 green = '\033[32m'
 yellow = '\033[33m'
-orange = '\033[33m   #\033[31m'
+orange = '\033[31m'
 blue = '\033[34m'
 magenta = '\033[35m'
 cyan = '\033[36m'
 
+ColorList = [red, green, blue, orange, magenta, yellow, cyan]
+
+os.system("clear")
+time.sleep(0.1)
+
+# ----- Print LOGO ----- #
+Logo = DoNKModule.ll
+print(red, Logo, reset)
+time.sleep(0.1)
+# ----- Print LOGO ----- #
+
+
 # Check OS
 os_info = platform.system()
-print(f"Detected : {os_info} OS")
-if is_info =="Linux": 
+print("-"*23)
+print(f"- {magenta}Detected : {os_info} OS{reset} -")
+print("-"*23)
+
+if os_info =="Linux": 
     # Clear terminal
-    os.system("clear")
-    time.sleep(1)
-    pass
+    # os.system("clear")
+    time.sleep(0.2)
 else:
-    print(f"Detected : {os_info} OS")
-    print("Aborting ... CUZ OS IS NOT LINUX\nNO SUPPORT FOR SCRIPTS :/")
+    print(f"{yellow}Detected : {red} {os_info} OS{reset}")
+    print(f"{red}Aborting ...{reset} CUZ OS IS NOT LINUX\nNO SUPPORT FOR SCRIPTS :/")
 
-    # ----------
+    # -------------------------------------------------------- #
     # Spravit triedu alebo func v "DoNKModul" pre SWAG vyhodenie
-    DoNKModul.Vyhadzovak()
-    # ----------
+    vyhodenie = DoNKModul.Vyhadzovak
+    print(vyhodenie)
+    # -------------------------------------------------------- #
 
 
-def check_monitor_mode():
-    # Spustí príkaz iwconfig a zachytí výstup ze neho musim nastavit text=False az to nepise vsetko
-    result = subprocess.run(["iwconfig"], capture_output=True, text=True)
+# --------------------------------------------------------------
+# SEM Check Mon mode ci je zapnuty na nejakom wlan WIFI adaptery
+# --------------------------------------------------------------
 
-    # Získa výstupné riadky z teho
-    output_lines = result.stdout.split('\n')
 
-    # Prejde cez riadky a vyhľada informácie o tom monitor móde
-    for line in output_lines:
-        if "Mode:Monitor" in line:
-            # Extrahujte názov wifi adaptéra
-            adapter_name = line.split()[0]
-            
-            # Vypíše informáciu o zapnutom monitor móde
-            print(f"Monitor mód je už zapnutý na: {adapter_name}")
-        else:
-          print("Monitor mod este neni zapnuty")
-# Spust funkciu pre kontrolu monitor módu
-print("Checking if Monitor Mode is Turned ON ...\n")
-check_monitor_mode()
+# --------------------------------
+# ------- Vypisanie Menu ---------
+Menu = DoNKModule.MainMenu
+RanColor = random.choice(ColorList)
+print(RanColor, Menu, reset)
+# --------------------------------
 
+UserInput = input(f"{yellow}Pick number --> {reset}")
+
+if UserInput =="1":
+    pass
 #spušta iba testovaci prikaz len tak :D
-os.system("iwconfig")
-AdapterChoice = input("Select WiFi Adapter for USE : ")
-print("Starting monitor mode on", AdapterChoice)
+# lol = subprocess.run(["iwconfig"], capture_output=True, text=False)
+# print(lol)
+
+# AdapterChoice = input("Select WiFi Adapter for USE : ")
+# print("Starting monitor mode on", AdapterChoice)
+# time.sleep(1)
+
+
+# -------- PRIKAZ PRE SPUSTENIE MON MODU ------- #
+# CommandForMonModeON = "sudo airmon-ng start"
+# MonModeON = subprocess.run([CommandForMonModeON, AdapterChoice], capture_output=True, text=True)
+# print(MonModeON)
+# -------------------- SEM --------------------- #
+
 time.sleep(1)
-CommandForMonModeON = "sudo airmon-ng start"
-subprocess.run([CommandForMonModeON, AdapterChoice], check=True, text=False, shell=False)
-time.sleep(1)
-subprocess.run([""])
+print("Spustanie prikazu ")
+#subprocess.run([""])
+
