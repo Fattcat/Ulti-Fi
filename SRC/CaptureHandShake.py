4c392cf057014935c401e3ee37c408271e96e8a8
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-01-05 14:19:29 +0000

    Update CaptureHandShake.py

--- SRC/CaptureHandShake.py
+++ SRC/CaptureHandShake.py
@@ -1,21 +1,24 @@
-# Capture HandShake file going to be here
-
+# ------ [ Moduly ] ------ #
+import os
+import time
 import subprocess
+# ------ [ Moduly ] ----- #
 
-while True:
-    # Input the target WiFi network
-    wifi_ssid = input("Enter the target WiFi SSID: ")
 
-    # Run airodump-ng to capture the handshake without terminal output
-    subprocess.run(f"airodump-ng --essid {wifi_ssid} -w capture_file wlan0", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+def ShakeIt():
+    # ----- [ Inputy ]  ----- #
+    ChannelInput = str(input("Write WiFi Channel : "))
+    TargetMacAddress = str(input("Write MacAddress of Target WiFi : "))
+    # ------ [ Inputy ]  ----- #
 
-    # Once you capture the handshake, stop the process without terminal output
-    result = subprocess.run("aircrack-ng capture_file-01.cap", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # ------ [ Catching HandShake ]  ----- #
+    CaptureHand = f"sudo airodump-ng -c{ChannelInput} -d {TargetMacAddress} wlan1"
+    CatchIt = subprocess.run([CaptureHand], capture_output=True, text=True, shell=True)
+    output = CatchIt.stdout
+    if "handshake" in output:
+        print("Sucessfully Captured WiFi HandShake file")
+    else:
+        pass
 
-    # Check if handshake capture was successful
-    if "1 handshake" in result.stdout.decode():
-        # Save the handshake to a file
-        subprocess.run(f"mv capture_file-01.cap {wifi_ssid}_handshake.cap", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-        print(f"Handshake capture for {wifi_ssid} successful!")
-        break  # Exit the loop once handshake is captured
+    # ------ [ Catching HandShake ]  ----- #
